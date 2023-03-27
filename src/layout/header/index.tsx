6d2b8834e50d8f113cdf1f9f2fa876c56ae8e67b
Hoang Minh Duc <duchm.bhsoft@gmail.com>
2023-03-27 11:27:01 +0700

    feat(view): home page - Top rated movies,Trending TV,Trending Movies

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -6,41 +6,37 @@ import Image from 'next/image';
 const Header = () => {
     const router = useRouter();
     return (
-        <header className="fixed top-0 w-full z-10">
-            <div>
-                <nav
-                    className="
+        <header
+            className="
           flex flex-wrap
           items-center
           justify-between
-          w-full	
           h-20
+          fixed top-0 w-full z-10
         "
-                >
-                    <div className="md:p-4 py-2 block text-white hover:text-red-600 font-bold text-[24px]">
-                        <a href="#" onClick={() => router.push('/')}>
-                            Home
-                        </a>
-                    </div>
-                    <div className="hidden w-full md:flex md:items-center md:w-auto " id="menu">
-                        <ul
-                            className="
+        >
+            <div className="md:p-4 py-2 block text-white hover:text-red-600 font-bold text-[24px]">
+                <a href="#" onClick={() => router.push('/')}>
+                    Home
+                </a>
+            </div>
+            <div className="hidden w-full md:flex md:items-center md:w-auto " id="menu">
+                <ul
+                    className="
               pt-4
               text-[24px] text-gray-700
               md:flex
               md:justify-between 
               md:pt-0"
-                        >
-                            {menu.map((items) => (
-                                <li key={items.index}>
-                                    <a className="py-3 block text-white hover:text-red-600 font-bold hover:border-b-2 mx-4 text-[24px]" href="#" onClick={() => router.push(items.path)}>
-                                        {items.name}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </nav>
+                >
+                    {menu.map((items) => (
+                        <li key={items.index}>
+                            <a className="py-3 block text-white hover:text-red-600 font-bold hover:border-b-2 mx-4 text-[24px]" href="#" onClick={() => router.push(items.path)}>
+                                {items.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </header>
     );
